3c10f425e5e4d64201e1ac5b20b894143600c27e
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-05-17 18:21:42 -0300

    my first App with Kotlin

---
+++ app/src/main/java/com/example/firstaplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.firstaplication
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun sorted(view: View){
+        val textResult = findViewById<TextView>(R.id.result_text)
+        val numero = java.util.Random().nextInt(11) // 0 ... 10
+        textResult.setText("$numero")
+    }
+}
